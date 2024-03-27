75a24ae73fc6e861dfb4a0602fd8cb6a3b391c98
MeisterSpooky <schafer.javier@gmail.com>
2024-03-27 16:25:45 -0600

    Added images folder and profile pic

--- content/about.php
+++ content/about.php
@@ -3,7 +3,7 @@
 <ul class="team-members">
   <?php
   $teamMembers = [
-    ['name' => 'Javier Schafer', 'biography' => 'Hello! I am Javier and am a soon to be graduate from the CS program at CSU! I have a passion for IT Security and plan on pursuing that after I graduate in May.', 'image' => 'member1.jpg'],
+    ['name' => 'Javier Schafer', 'biography' => 'Hello! I am Javier and am a soon to be graduate from the CS program at CSU! I have a passion for IT Security and plan on pursuing that after I graduate in May.', 'image' => 'images/schaferj.jpg'],
     ['name' => 'Knott Alex', 'biography' => 'Brief biography.', 'image' => 'member2.jpg'],
     ['name' => 'Alejando Marmolejo-Daher', 'biography' => 'Brief biography.', 'image' => 'member3.jpg'],
     ['name' => 'Lukas Elerson', 'biography' => 'Brief biography.', 'image' => 'member4.jpg']
