a0e7cb79e45224af1e9462ada6a7191bace2d840
Alberto Marmolejo-Daher <jokitjo@pierre.cs.colostate.edu>
2024-04-05 10:01:39 -0600

    added biography

--- content/about.php
+++ content/about.php
@@ -5,7 +5,7 @@
   $teamMembers = [
     ['name' => 'Javier Schafer', 'biography' => 'Hello! I am Javier and am a soon to be graduate from the CS program at CSU! I have a passion for IT Security and plan on pursuing that after I graduate in May.', 'image' => './content/images/schaferj.jpg'],
     ['name' => 'Knott Alex', 'biography' => 'Brief biography.', 'image' => './content/images/placeholder.jpg'],
-    ['name' => 'Alejando Marmolejo-Daher', 'biography' => 'Brief biography.', 'image' => './content/images/placeholder.jpg'],
+    ['name' => 'Alberto Marmolejo-Daher', 'biography' => 'Hello! My name is Alberto and I like eating empanadas, I am a Junior in the CS program!', 'image' => './content/images/dumdog.png'],
     ['name' => 'Lukas Elerson', 'biography' => 'Brief biography.', 'image' => './content/images/placeholder.jpg']
   ];
   
